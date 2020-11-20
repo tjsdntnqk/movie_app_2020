6acae4fcc935634872ef75f5f81fcec95c21f390
unknown <tjsdntnqk@naver.com>
2020-11-20 16:55:25 +0900

    Updates

---
+++ precache-manifest.ab7dbb676f6962fc1be36bb71a45c466.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "357f550b41e818eb4fc1d9e573b8bc14",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "a53f7cfe0eb0e6cc6a99",
+    "url": "/movie_app_2020/static/css/main.9e6971ca.chunk.css"
+  },
+  {
+    "revision": "1659a2f20802a3b78ff3",
+    "url": "/movie_app_2020/static/js/2.21bef84b.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/movie_app_2020/static/js/2.21bef84b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a53f7cfe0eb0e6cc6a99",
+    "url": "/movie_app_2020/static/js/main.2f633b5f.chunk.js"
+  },
+  {
+    "revision": "4764f725c29e46abd49d",
+    "url": "/movie_app_2020/static/js/runtime-main.917b8485.js"
+  }
+]);
