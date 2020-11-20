1bc094dd6a8d0a48ba441f60bef46f59c930b494
unknown <tjsdntnqk@naver.com>
2020-11-20 16:33:51 +0900

    movie_app_2020

---
+++ src/components/Movie.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Movie.css';
+import { Link } from 'react-router-dom';
+
+function Movie({year, title, summary, poster, genres, rating}) {
+    return (
+    <div className="movie">
+       <Link
+          to={{
+              pathname: '/movie-detail',
+              state: { year, title, summary, poster, genres, rating },
+          }}
+       >           
+       <img src={poster} alt={title} title={title} />
+       <div className="movie-data">        
+       <h3 className="movie-title">{title}</h3>
+       <h5 className="movie-year">{year}</h5>
+       <h4 className="movie-rating">{rating}</h4>
+       <ul className="movie-genres">
+        {genres.map((genre, index) => {
+            return <li key={index} className="movie-genres">{genre}</li>;
+        })}
+       </ul>
+       <p className="movie-summary">{summary.slice(0, 180)}...[more]</p>
+     </div>
+    </Link>
+    </div>
+    );
+}
+
+Movie.propTypes = {
+    year: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string),
+};
+
+export default Movie;
---
+++ src/routes/Detail.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class Detail extends React.Component {
+    componentDidMount() {
+        const { location, history } = this.props;
+
+        if(location.state === undefined) {
+            history.push('/');
+        }
+    }
+
+    render() {
+        const { location } = this.props
+        if (location.state) {
+        return (
+        <div>
+        <img src={location.state.poster} alt={location.state.title}/>
+        <h1> {location.state.title}</h1>
+        <h3> {location.state.year}</h3>
+        <h3> {location.state.rating}</h3>
+        <h3> {location.state.genres}</h3>        
+        </div>
+        );
+        } else {
+            return null;
+        }
+    }
+}
+
+export default Detail;
